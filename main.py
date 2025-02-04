3a00eaa60f3e60182e90cee519bffafb8d2b119e
AdresGezgini Inc. <github@adresgezgini.com>
2025-02-04 18:34:29 +0100

    Add data retrieval from external source and save to file

--- main.py
+++ main.py
@@ -4,7 +4,9 @@
 import sys
 import os
 import time 
+import requests
 
+URL="http://www.koeri.boun.edu.tr/scripts/lst2.asp"
 
 # main function
 def main():
@@ -13,8 +15,20 @@ def main():
     # check whether there is a folder for the year
     if not os.path.exists(year):
         os.makedirs(year)
-    # check whether there is a folder for the month    
+    # check whether there is a folder for the month
 
+    response = requests.get(URL)
+    response.encoding = "windows-1254"  # Ensure correct encoding
+    
+    if response.status_code != 200:
+        print("Failed to retrieve data")
+        return None
+    else:
+        print("Data retrieved successfully")
+        # write the data to a file
+        with open(f"{year}/data.txt", "w") as file:
+            file.write(response.text)
+        
     return None
 
 # main function
