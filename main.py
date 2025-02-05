400963037567e04cc365cf22d64b58de1d763c02
AdresGezgini Inc. <github@adresgezgini.com>
2025-02-05 07:34:38 +0100

    Update file path for earthquake data storage and remove redundant save operation

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def fetch_earthquake_data():
             # Convert date to extract year and month
             date_obj = datetime.datetime.strptime(date, "%Y.%m.%d")
             year_folder = str(date_obj.year)
-            month_file = os.path.join(year_folder, f"{date_obj.strftime('%m')}.csv")
+            month_file = os.path.join("/home/GitHub/deprem",year_folder, f"{date_obj.strftime('%m')}.csv")
 
             earthquakes.append([date, time, latitude, longitude, depth, md, ml, mw, location, year_folder, month_file])
 
@@ -82,7 +82,7 @@ def fetch_earthquake_data():
         
 
         # Save updated data ensure overwriting
-        combined_df.to_csv(month_file, index=False)
+
         
         print(f"Updated {month_file} with new earthquake data.")
         print(combined_df.tail(20))
